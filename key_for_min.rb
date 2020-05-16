176e93ec7611891063b9976360eb830e897f8fc2
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-16 08:48:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   minKey = name_hash[0][0]
   name_hash.each do |key, value|
     if value <= minKey
-      minkey = value
+      minkey = key
     end
   end
   minKey
